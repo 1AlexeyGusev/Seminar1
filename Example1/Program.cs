caa2f491878b2a99530271bb81c25578119a1e5b
AlexG <gusev_alexey@mail.ru>
2023-09-19 22:12:59 +0300

    exii1

---
+++ Example1/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int str = number * number;
+
+//Вывод построчно
+Console.Write("Квадрат числа: ");
+Console.Write(number);
+Console.Write(" равен = ");
+Console.Write(str);
+
+
+//Вывод с использованием символа $
+Console.WriteLine($"Квадарт числа {number} равен = {str}");
+
+
+//Вывод с обращением к переменной
+Console.WriteLine("Квадарт числа {0} равен = {1}", number, str);
+
+
+//Использование библиотеки Math
+int sqr = Convert.ToInt32(Console.ReadLine());
+double sqr_2 = Math.Pow(sqr,2);
+Console.WriteLine("Квадарт числа {0} равен = {1}", sqr, sqr_2);
