4bb37ee9044d614f5afa83abb1b7c4a19ae8b845
AlexG <gusev_alexey@mail.ru>
2023-09-24 21:43:19 +0300

    task005,007

---
+++ Task005/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая на входе принимает число N, а потом выводит целые числа из промежутка от -N до N
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+int num = n*(-1);
+if(n > 0)
+{
+    while(num <= n)
+    {
+        Console.Write($"{num};");
+        num++;
+    }
+}
+
+else
+{
+    while(num >= n)
+    {
+        Console.Write($"{num};");
+        num--;
+    }
+}
---
+++ Task007/Program.cs
@@ -0,0 +1,5 @@
+﻿//Напишите программу, которая принимает на вход трехзначное число и выводит последнюю цифру данного числа
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+int Z = n%10;
+Console.WriteLine(Z);
