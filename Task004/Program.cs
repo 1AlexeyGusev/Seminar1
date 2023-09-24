4aee0160b5216b9a14be1fef0a50f760703cb862
AlexG <gusev_alexey@mail.ru>
2023-09-24 23:33:06 +0300

    task004 DZ

---
+++ Task004/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Write("Введите число а: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите число b: ");
+int b = int.Parse(Console.ReadLine());
+Console.Write("Введите число c: ");
+int c = int.Parse(Console.ReadLine());
+
+if(a > b)
+{
+    if(a > c)
+    {
+        Console.WriteLine($"Максимальное число {a}");
+    }
+    else
+    {
+        Console.WriteLine($"Максимальное число {c}");
+    }
+}
+else if(b > c)
+{
+    Console.WriteLine($"Максимальное число {b}");
+}
+else
+{
+    Console.WriteLine($"Максимальное число {c}");
+}
