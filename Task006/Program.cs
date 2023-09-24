143e142f75304101bc75ac52f77d7c4226b1a3f9
AlexG <gusev_alexey@mail.ru>
2023-09-24 23:39:29 +0300

    task006 DZ

---
+++ Task006/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+if(a % 2 == 0)
+{
+    Console.WriteLine("Число четно!");
+}
+else
+{
+    Console.WriteLine("Число нечетно!");
+}
