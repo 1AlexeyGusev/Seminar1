6a035977f3cb496b0ce065b74a79a3cea42f9e59
AlexG <gusev_alexey@mail.ru>
2023-09-24 22:42:53 +0300

    task002 DZ

---
+++ Task002/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите число а: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите число b: ");
+int b = int.Parse(Console.ReadLine());
+
+if(a > b)
+{
+    Console.WriteLine($"Число {a} > числа {b}");
+}
+else if(a < b)
+{
+    Console.WriteLine($"Число {a} < числа {b}");
+}
+else
+{
+    Console.WriteLine("Числа равны");
+}
