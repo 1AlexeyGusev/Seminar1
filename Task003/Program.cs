0af928a888516d18811df3420d17fd97f7118ca2
AlexG <gusev_alexey@mail.ru>
2023-09-24 19:38:25 +0300

    ex003

---
+++ Task003/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+if(number == 1)
+{
+    Console.WriteLine("Сегодня понедельник!");
+}
+else if(number == 2)
+{
+    Console.WriteLine("Сегодня вторник!");
+}
+else if(number == 3)
+{
+    Console.WriteLine("Сегодня среда!");
+}
+else if(number == 4)
+{
+    Console.WriteLine("Сегодня четверг!");
+}
+else if(number == 5)
+{
+    Console.WriteLine("Сегодня пятница!");
+}
+else if(number == 6)
+{
+    Console.WriteLine("Сегодня суббота!");
+}
+else if(number == 7)
+{
+    Console.WriteLine("Сегодня воскресенье!");
+}
+else
+{
+    Console.WriteLine("Введите число от 1 до 7!");
+}
