593960edea975bf8fa7dbaa0a71a7e5e76dac4b1
AlexG <gusev_alexey@mail.ru>
2023-09-24 22:33:34 +0300

    005,007

--- Task007/Program.cs
+++ Task007/Program.cs
@@ -1,5 +1,18 @@
-﻿//Напишите программу, которая принимает на вход трехзначное число и выводит последнюю цифру данного числа
+﻿// //Напишите программу, которая принимает на вход трехзначное число и выводит последнюю цифру данного числа
+// Console.WriteLine("Введите число: ");
+// int n = int.Parse(Console.ReadLine());
+// int Z = n%10;
+// Console.WriteLine(Z);
+
 Console.WriteLine("Введите число: ");
-int n = int.Parse(Console.ReadLine());
-int Z = n%10;
-Console.WriteLine(Z);
+string number = Console.ReadLine();
+int amount = number.Length;
+
+if(amount != 3)
+{
+    Console.WriteLine("Введите ТРЕХзначное число!");
+}
+else    
+{
+    Console.WriteLine(number.Substring(amount-1));
+}
