3ad727eebf8374a453f2f0915627f2b2f823938f
AlexG <gusev_alexey@mail.ru>
2023-09-24 22:08:48 +0300

    Task005

--- Task005/Program.cs
+++ Task005/Program.cs
@@ -1,21 +1,35 @@
-﻿// Напишите программу, которая на входе принимает число N, а потом выводит целые числа из промежутка от -N до N
-Console.WriteLine("Введите число: ");
-int n = int.Parse(Console.ReadLine());
-int num = n*(-1);
-if(n > 0)
-{
-    while(num <= n)
-    {
-        Console.Write($"{num};");
-        num++;
-    }
-}
+﻿// // Напишите программу, которая на входе принимает число N, а потом выводит целые числа из промежутка от -N до N
+// Console.WriteLine("Введите число: ");
+// int n = int.Parse(Console.ReadLine());
+// int num = n*(-1);
+// if(n > 0)
+// {
+//     while(num <= n)
+//     {
+//         Console.Write($"{num};");
+//         num++;
+//     }
+// }
 
-else
+// else
+// {
+//     while(num >= n)
+//     {
+//         Console.Write($"{num};");
+//         num--;
+//     }
+// }
+//------------------------------------------------------------------
+
+
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+Math.Abs(a);
+int i = Math.Abs(a) * (-1);
+do
 {
-    while(num >= n)
-    {
-        Console.Write($"{num};");
-        num--;
-    }
+    Console.Write($"{i}, ");
+    i++;
 }
+    while(i <= Math.Abs(a));
+
