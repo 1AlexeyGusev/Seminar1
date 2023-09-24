9723aa92a341f49b74ee03e7856d55d9c9828739
AlexG <gusev_alexey@mail.ru>
2023-09-24 19:59:36 +0300

    Task001 add second var

--- Task001/Program.cs
+++ Task001/Program.cs
@@ -1,10 +1,19 @@
-﻿int a = 25;
-int b = 6;
-if(a == b * b)
-{
-    Console.WriteLine("Верно");
-}
-else
-{
-    Console.WriteLine("Неверно");
-}
+﻿// int a = 25;
+// int b = 6;
+// if(a == b * b)
+// {
+//     Console.WriteLine("Верно");
+// }
+// else
+// {
+//     Console.WriteLine("Неверно");
+// }
+
+Console.Write("Введите число а: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("Введите число b: ");
+int b = int.Parse(Console.ReadLine());
+
+bool c = a == b * b;
+Console.WriteLine($"Число {a} является квадратом числа {b} - {c}");
